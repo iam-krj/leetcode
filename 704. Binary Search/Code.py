b931a2efca9760d9768f147052cac93c1bceb244
Krishna Raj <75441878+iam-krj@users.noreply.github.com>
2022-03-15 23:49:02 +0530

    LeetCode Problem

---
+++ 704. Binary Search/Code.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+     # if len(nums)==1:
+     left = 0
+     right = len(nums)-1
+     while left<=right:
+        mid = (left + right) / 2
+        mid = int(mid)
+        if nums[mid] == target:
+           
+            return mid
+        elif nums[mid] < target:
+          
+            left = mid+1
+          
+        else:
+          
+            right = mid-1
+     return -1
+ 
