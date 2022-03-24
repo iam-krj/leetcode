b4ab581b6b7bd902071e713f0849c2b8942b8bb3
Krishna Raj <75441878+iam-krj@users.noreply.github.com>
2022-03-24 20:40:44 +0530

    LeetCode Problem 21

---
+++ 21. Merge Two Sorted Lists/Code.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        head = p =ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                p.next = l1
+                l1 = l1.next
+            else:
+                p.next = l2
+                l2 = l2.next
+            p = p.next
+        if l1:
+            p.next = l1
+        else:
+            p.next = l2
+        return head.next
